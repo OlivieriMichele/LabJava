fe72bb07db252e745a6dbe0626f9bd24af1d7b35
Michele Olivieri <micheleolivieri02@gmail.com>
2023-12-19 12:13:38 +0100

    solution of gui a02a.e2

--- a02a/e2/GUI.java
+++ a02a/e2/GUI.java
@@ -2,32 +2,37 @@ package a02a.e2;
 
 import javax.swing.*;
 import java.util.*;
-import java.util.List;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
     
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
+    private final Map<Pair<Integer,Integer>,JButton> cells = new HashMap<>();
+    private final Logics logic;
     private int counter = 0;
     
     public GUI(int size) {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(50*size, 50*size);
+        this.logic = new LogicsImpl(size);
         
         JPanel panel = new JPanel(new GridLayout(size,size));
         this.getContentPane().add(panel);
         
         ActionListener al = e -> {
-        	this.cells.get(counter).setText(String.valueOf(counter++));
+            var p = logic.next();
+            if (p.isEmpty()) {
+                System.exit(0);
+            }
+        	this.cells.get(p.get()).setText(String.valueOf(counter++));
         };
                 
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
             	var pos = new Pair<>(j,i);
                 final JButton jb = new JButton(" ");
-                this.cells.add(jb);
+                this.cells.put(pos,jb);
                 jb.addActionListener(al);
                 panel.add(jb);
             }
